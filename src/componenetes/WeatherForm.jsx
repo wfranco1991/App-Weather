cee1218d65c4a5c18efec5284ecda09dfbd90982
Wilmar Franco <wfranco19r@gmail.com>
2023-12-10 06:12:34 -0500

    se terminan las validaciones de input y se les dan estilo

--- src/componenetes/WeatherForm.jsx
+++ src/componenetes/WeatherForm.jsx
@@ -8,27 +8,27 @@ import swal from "sweetalert";
 
 const WeatherForm = () => {
   const [city, setCity] = useState("Medellin");
-  const { weather, apiCity } = useOpenWeather();
+  const { weather, apiCity, error } = useOpenWeather();
   const fechaActual = new Date(/* weather?.dt*1000 */).toLocaleString();
 
 
   const buscar = () => {
-   if (city === ""){
-    swal({
-      title: "Campo vacio",
-      text: "Ingresa una Ciudad en la barra de busqueda",
-      icon: "warning"
-    });
-   }else{
-    apiCity(city);
-    }
-}
 
+    if (city === "") {
+      swal({
+        title: "Campo vacio",
+        text: "Ingresa una Ciudad en la barra de busqueda",
+        icon: "warning"
+      });
+      return false
+    }
+    apiCity(city)
+  }
   useEffect(() => {
     apiCity(city);
   }, []);
 
-  
+
 
   return (
     <>
@@ -43,44 +43,56 @@ const WeatherForm = () => {
         />
         <IoSearchSharp id="lupa" />
         <button className="button" onClick={buscar}></button>
-    
+
 
         <h1 className="titulo">App del Clima</h1>
-        <img src="/src/IMG/calido.png" alt="icono clima" className="icono"/>
+        <img src="/src/IMG/calido.png" alt="icono clima" className="icono" />
       </header>
-      <h2 className="city">El clima en: {city}</h2>
-      <main className="contenedor">
-        <section className="tiempoActual">
-          <h3 id="tAcutal">Tiempo actual</h3>
-          <h5 className="fechaYa">{fechaActual}</h5>
-          <img
-            className="img"
-            src={`https://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`}
-            alt="system icon"
-          />
-          {weather && (
-            <div className="clima">
-              <p id="temperatura">{weather.main.temp} °C</p>
-              <span className="cardYa">Humedad: {weather.main.humidity}%</span>
-              <span className="cardYa">
-                Presión Atmosférica: {weather.main.pressure} hPa
-              </span>
-              <span className="cardYa">
-                Sensacion Termica: {weather.main.feels_like} °C
-              </span>
-              <span className="cardYa">
-                Velocidad del Viento: {weather.wind.speed} km/h
-              </span>
-            </div>
-          )}
-        </section>
-        <Maps
-          weather={weather}
-          latitude={weather?.coord?.lat}
-          longitude={weather?.coord?.lon}
-        />
-        <Pronostico city={city} weather={weather} temperature={"temperature"} />
-      </main>
+      {
+        error
+          ?
+          
+          <p className="ciudadMala" ><img src="/src/IMG/error.png" alt="error ciudad" className="errorCity"/>
+           Ciudad no encontrada: {city}<p className="ciudadMala2">
+            Ingrese una Ciudad valida en la barra de busqueda...
+           </p></p>
+          :
+          <div>
+            <h2 className="city">El clima en: {city}</h2>
+            <main className="contenedor">
+              <section className="tiempoActual">
+                <h3 id="tAcutal">Tiempo actual</h3>
+                <h5 className="fechaYa">{fechaActual}</h5>
+                <img
+                  className="img"
+                  src={`https://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`}
+                  alt="system icon"
+                />
+                {weather && (
+                  <section className="clima">
+                    <p id="temperatura">{weather.main.temp} °C</p>
+                    <span className="cardYa">Humedad: {weather.main.humidity}%</span>
+                    <span className="cardYa">
+                      Presión Atmosférica: {weather.main.pressure} hPa
+                    </span>
+                    <span className="cardYa">
+                      Sensacion Termica: {weather.main.feels_like} °C
+                    </span>
+                    <span className="cardYa">
+                      Velocidad del Viento: {weather.wind.speed} km/h
+                    </span>
+                  </section>
+                )}
+              </section>
+              <Maps
+                weather={weather}
+                latitude={weather?.coord?.lat}
+                longitude={weather?.coord?.lon}
+              />
+              <Pronostico city={city} weather={weather} temperature={"temperature"} />
+            </main>
+          </div>
+      }
     </>
   );
 };
--- src/hooks/useOpenWeather.js
+++ src/hooks/useOpenWeather.js
@@ -3,29 +3,35 @@ import { useState } from "react";
 const useOpenWeather = () => {
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState(null);
-  
 
-  const apiKey = "c2b51d6c5e9cbb27fd30667c429e7721";
+
+  /* const apiKey = "c2b51d6c5e9cbb27fd30667c429e7721"; */
+  const apiKey = import.meta.env.VITE_API_KEY
 
   const apiCity = (city) => {
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     )
-      .then((response) => response.json())
-      .then((datos) => {
+      .then(response => response.json())
+      .then(datos => {
+        if (datos.cod === '404') {
+          setError(datos.message)
+          console.log(error)
+          return false
+        }
         setError(null);
         setWeather(datos);
-        
-      /*   console.log(datos); */
+
+        console.log(datos);
       })
       .catch((error) => {
-        setWeather(null);
+        /* setWeather(null); */
         setError(error);
       });
   };
 
 
 
-  return {weather, error, apiCity};
+  return { weather, error, apiCity };
 };
 export default useOpenWeather;
